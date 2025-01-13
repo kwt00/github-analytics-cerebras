1426944ed49813765bfdd3dea5930bedf850547b
kwt00 <116334641+kwt00@users.noreply.github.com>
2025-01-13 12:30:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -343,7 +343,8 @@ async function getReactions(startDate, endDate) {
 
 async function getProjectLinks(startDate, endDate) {
     const messages = await getAllChannelMessages(CHANNEL_ID, startDate, endDate);
-    const urlRegex = /(https?:\/\/[^\s<>"]+|www\.[^\s<>"]+)/g;
+    const urlRegex = /(https?:\/\/[^\s<>"]+|www\.[^\s<>"]+|http?:\/\/[^\s<>"]+)/g;
+    
     const projectLinks = new Set();
 
     messages.forEach(msg => {
