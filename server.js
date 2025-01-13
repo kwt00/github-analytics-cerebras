3fe6dd35121dc8409167ba8a112ca740d45af060
kwt00 <116334641+kwt00@users.noreply.github.com>
2025-01-13 12:54:17 -0800

    Update server.js

--- server.js
+++ server.js
@@ -343,7 +343,7 @@ async function getReactions(startDate, endDate) {
 
 async function getProjectLinks(startDate, endDate) {
     const messages = await getAllChannelMessages(CHANNEL_ID, startDate, endDate);
-    const urlRegex = /(https?:\/\/[^\s<>"]+|www\.[^\s<>"]+|http?:\/\/[^\s<>"]+)/g;
+    const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
     
     const projectLinks = new Set();
 
